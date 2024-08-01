41949fd9bd84c35d096b4dc8ff6ac3e6e9c70c99
szymon5922 <szymon20005920@gmail.com>
2024-08-01 10:08:07 +0200

    solution update

--- Solution.cs
+++ Solution.cs
@@ -3384,5 +3384,67 @@ namespace Solutions
 
             return nums;
         }
+        #region MinHeightShelves
+        public static int MinHeightShelves(int[][] books, int shelfWidth)
+        {
+            int[][] memo = new int[books.Length][];
+            for (int i = 0; i < memo.Length; i++)
+            {
+                memo[i] = new int[shelfWidth + 1]; 
+            }
+            return dpHelper(books,shelfWidth,memo,0,shelfWidth,0);
+        }
+        private static int dpHelper(
+            int[][] books,
+            int shelfWidth,
+            int[][] memo,
+            int i,
+            int remainingShelfWidth,
+            int maxHeight)
+        {
+            int[] currentBook = books[i];
+            int maxHeightUpdated = Math.Max(maxHeight, currentBook[1]);
+
+            if(i==books.Length-1)
+            {
+                if(remainingShelfWidth >= currentBook[0])
+                    return maxHeightUpdated;
+                else
+                    return maxHeight+currentBook[1];
+            }
+
+            if (memo[i][remainingShelfWidth] != 0)
+                return memo[i][remainingShelfWidth];
+            else
+            {
+                int option1Height =
+                    maxHeight + dpHelper(
+                        books,
+                        shelfWidth,
+                        memo,
+                        i + 1,
+                        shelfWidth - currentBook[0],
+                        currentBook[1]);
+
+                if(remainingShelfWidth >= currentBook[0])
+                { 
+                    int option2Height = dpHelper(
+                        books,
+                        shelfWidth,
+                        memo,
+                        i + 1,
+                        remainingShelfWidth - currentBook[0],
+                        maxHeightUpdated);
+
+                    memo[i][remainingShelfWidth] = Math.Min(option1Height, option2Height);
+
+                    return memo[i][remainingShelfWidth];
+                }
+                
+                memo[i][remainingShelfWidth] = option1Height;
+                return memo[i][remainingShelfWidth];
+            }
+        }
+        #endregion
     }
 }
